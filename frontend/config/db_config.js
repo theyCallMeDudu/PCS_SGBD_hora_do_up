979855af13826ca01febe382f27cd22c69f94352
Eduardo Coelho <coelho.edu96@gmail.com>
2022-07-06 21:39:26 -0300

    Adicionando configurações mysql.

---
+++ frontend/config/db_config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "123456",
+    DB: "hora_do_up",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+};
