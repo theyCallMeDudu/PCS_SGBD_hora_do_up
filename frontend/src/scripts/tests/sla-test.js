82eb7f633f839cfec4c5a51abce17310c7adf2e6
Eduardo Coelho <coelho.edu96@gmail.com>
2022-07-09 12:18:12 -0300

    Atualizando script de testes SLA.

--- frontend/src/scripts/tests/sla-test.js
+++ frontend/src/scripts/tests/sla-test.js
@@ -1,74 +1,78 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
 
-// Common things
+// Configurações API K6
 const API_URL = "https://gorest.co.in/public-api";
 const API_TOKEN = "2265725b6084f455bb578df78606a68cd24ebf8ead5d25e811bd9638c4371b91";
 
-// Test setup
+// Configurações dos testes
 export let options = {
     vus: 10,
     duration: '30s'
 };
 
-// Test scenario
+// Cenário de testes
 export default function () {
 
-    // Common requests params
+    // Parâmetros comuns à diversas requisições
     const params = { headers: { "Authorization": `Bearer ${API_TOKEN}` } };
 
-    // Random e-mail suffix
+    // Geração de sufixo de e-mail aleatório
     let randomInt = Math.floor(Math.random() * 1000000);
 
-    // Create user
+    // Geração de id aleatório
+    function randomId(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
+    //////////////////////////////////////////////////////////////////////
+
+    /*
+    * Funcionalidades testadas:
+    * . Serviço 1 [ESCRITA]: Criar conta (objetivo: permitir o cadastro de usuários no sistema)
+    * . Serviço 2 [LEITURA]: Exibir conta (objetivo: permitir o usuário consultar dados da sua conta no sistema)
+    */
+
+    //////////////////////////////////////////////////////////////////////
+
+    // Serviço 1 [ESCRITA]: Criar conta
     let createUserResponse = http.post(
         `${API_URL}/users`,
         {
-            "name": "John Doe",
-            "gender": "Male",
-            "email": `john.doe+${randomInt}@tsh.io`,
-            "status": "Active"
+            "nick": "Usuário Teste",
+            "email": `usuario.teste+${randomInt}@mail.com`,
+            "senha": "password",
+            "senhaCheck": "password",
+            "uid": `${randomId(28)}`
         },
         params
     );
     check(
         createUserResponse,
-        { "Create user response status code is 200": (r) => r.status == 200 }
+        { "Status code 200 retornado": (r) => r.status == 200 }
     );
 
-    // Get user 
+    //////////////////////////////////////////////////////////////////////
+
+    // Serviço 2 [LEITURA]: Exibir conta 
     let getUserResponse = http.get(
         `${API_URL}/users/${JSON.parse(createUserResponse.body).data.id}`,
         params
     )
     check(
         getUserResponse,
-        { "Get user response status code is 200": (r) => r.status == 200 }
+        { "Status code 200 retornado": (r) => r.status == 200 }
     );
 
-    // Update user data
-    let patchUserResponse = http.patch(
-        `${API_URL}/users/${JSON.parse(createUserResponse.body).data.id}`,
-        {
-            "gender": "Female"
-        },
-        params
-    )
-    check(
-        patchUserResponse,
-        { "Update user response status code is 200": (r) => r.status == 200 }
-    );
+    //////////////////////////////////////////////////////////////////////
 
-    // Delete user
-    let deleteUserResponse = http.del(
-        `${API_URL}/users/${JSON.parse(createUserResponse.body).data.id}`,
-        params
-    )
-    check(
-        deleteUserResponse,
-        { "Delete user response status code is 200": (r) => r.status == 200 }
-    );
-
-    // Short break between iterations
+    // Pausa entre operações
     sleep(0.5);
+    
 }
